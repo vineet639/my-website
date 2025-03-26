b4f0915b376d5561219574ff2d8ccb35ae648c12
vineet639 <123579605+vineet639@users.noreply.github.com>
2025-03-27 03:42:30 +0530

    Update urls.py

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Cosmonix!</h1>")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", home, name="home"),  # Default homepage
 ]
